f71e323ec817b61e92f10e4c362187f9b95969cd
morganmark <32056855+morganmark@users.noreply.github.com>
2019-12-31 00:13:35 +0800

    1

---
+++ Unity_106021013_Morgan/Assets/action/IdleRun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleRun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunAndIdle();
+        Jump();
+        Jumping();
+    }
+
+    public Animator ani;
+    private void RunAndIdle()
+    {
+        print(Input.GetAxis("Horizontal"));
+        print(Input.GetAxis("Vertical"));
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        ani.SetBool("IdleToRun", h != 0 || v != 0);
+    }
+    private void Jump()
+    {
+        ani.SetBool("IdleToJump", Input.GetKeyDown(KeyCode.Space));
+        ani.SetBool("IdleToJump", Input.GetKeyUp(KeyCode.Space));
+    }
+    private void Jumping()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        ani.SetBool("RunningToJumping", Input.GetKeyDown(KeyCode.Space)&& (h != 0 || v != 0));
+    }
+
+}
